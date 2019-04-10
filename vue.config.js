fabbc4a78a10f0d1f77bc2181094cab5f4d13178
jerryzhang <jerryzhang@muchenglin.com>
2019-04-10 12:35:13 +0800

    fix: 增加默认配置

--- vue.config.js
+++ vue.config.js
@@ -1,14 +1,23 @@
 const path = require('path')
 const fs = require('fs')
 const argv = require('minimist')(process.argv.slice(2))
-const projectName = path.resolve(__dirname).split(path.sep).pop()
-const env = process.env.NODE_ENV
-const isDev = env === 'development'
-const publicPath = process.env.VUE_APP_STATIC_URL
-const outputDir = `dist/${projectName}/`
 const productionSourceMap = !!argv.sourceMap
-const files = fs.readdirSync(path.join(__dirname, 'src/'))
-const indexPath = files.find(file => path.extname(file) === '.html')
+const isDev = process.env.NODE_ENV === 'development'
+const srcFiles = fs.readdirSync(path.resolve(__dirname, 'src/'))
+// 项目名称
+const projectName = path.resolve(__dirname).split(path.sep).pop()
+// 公共地址
+// 根据环境配置 在.env.development 和 .env.production 中配置
+const publicPath = process.env.VUE_APP_STATIC_URL
+// 生产环境构建文件的目录
+const outputDir = `dist/${projectName}/`
+// 获取配置
+const config = require('./config.js')
+// 页面片路径
+const indexPath = config.indexPath ||
+    srcFiles.find(file => path.extname(file) === '.html')
+// 入口路径 (相对路径)
+const entryPath = config.entryPath || './src/main.js'
 
 module.exports = {
     outputDir: outputDir,
@@ -32,6 +41,7 @@ module.exports = {
         }
     },
     configureWebpack: config => {
+        config.entry.app[0] = entryPath
         if (isDev) {
             config.output.filename = 'js/[name].js'
             config.output.chunkFilename = 'js/[name].js'
