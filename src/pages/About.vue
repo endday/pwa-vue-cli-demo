6a60978f909ea6c2e9e37c7c1487dde9c90f4922
jerryzhang <jerryzhang@muchenglin.com>
2019-04-24 14:41:03 +0800

    fix: 修改版本

--- src/pages/About.vue
+++ src/pages/About.vue
@@ -1,17 +1,9 @@
 <template>
-    <div class="about">
-        <h1>This is an about page</h1>
-    </div>
+  <div class="about">
+    <h1>This is an about page</h1>
+  </div>
 </template>
 <script>
-const a = [
-    'a',
-    'b',
-    'c'
-]
-
-const b = a.includes('a')
-console.log(b)
 export default {}
 </script>
 <style scoped lang="scss">
--- vue.config.js
+++ vue.config.js
@@ -15,36 +15,36 @@ const outputDir = `dist/${projectName}/`
 const config = require('./config.js')
 // 页面片路径
 const indexPath = config.indexPath ||
-    srcFiles.find(file => path.extname(file) === '.html')
+  srcFiles.find(file => path.extname(file) === '.html')
 // 入口路径 (相对路径)
 const entryPath = config.entryPath || './src/main.js'
 
 module.exports = {
-    outputDir: outputDir,
-    indexPath: indexPath,
-    publicPath: `${publicPath}/${projectName}/`,
-    lintOnSave: false,
-    runtimeCompiler: true,
-    productionSourceMap,
-    crossorigin: 'anonymous',
-    chainWebpack: config => {
-        config.plugin('html')
-            .tap(args => {
-                args[0].minify = false
-                args[0].filename = indexPath
-                args[0].template = path.join(__dirname, `src/${indexPath}`)
-                return args
-            })
-        if (isDev) {
-            config.plugins.delete('preload')
-            config.plugins.delete('prefetch')
-        }
-    },
-    configureWebpack: config => {
-        config.entry.app[0] = entryPath
-        if (isDev) {
-            config.output.filename = 'js/[name].js'
-            config.output.chunkFilename = 'js/[name].js'
-        }
+  outputDir: outputDir,
+  indexPath: indexPath,
+  publicPath: `${publicPath}/${projectName}/`,
+  lintOnSave: false,
+  runtimeCompiler: true,
+  productionSourceMap,
+  crossorigin: 'anonymous',
+  chainWebpack: config => {
+    config.plugin('html')
+      .tap(args => {
+        args[0].minify = false
+        args[0].filename = indexPath
+        args[0].template = path.join(__dirname, `src/${indexPath}`)
+        return args
+      })
+    if (isDev) {
+      config.plugins.delete('preload')
+      config.plugins.delete('prefetch')
     }
+  },
+  configureWebpack: config => {
+    config.entry.app[0] = entryPath
+    if (isDev) {
+      config.output.filename = 'js/[name].js'
+      config.output.chunkFilename = 'js/[name].js'
+    }
+  }
 }
