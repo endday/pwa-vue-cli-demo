6279c79cea338dce8b3d66264d2322f7ba5f5a3b
邓建伟 <david@muchenglin.com>
2019-06-19 16:08:49 +0800

    更新 postcss.config.js

--- postcss.config.js
+++ postcss.config.js
@@ -2,7 +2,7 @@ module.exports = {
   plugins: {
     autoprefixer: {}/*,
     'postcss-pxtorem-muc': {
-      rootValue: 20,
+      rootValue: 10,
       unitPrecision: 5,
       propList: ['*', '!border', '!border-bottom', '!border-top', '!border-right', '!border-left', '!max-width', '!min-width', '!min-height', '!max-height'],
       selectorBlackList: [],
