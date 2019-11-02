eb799efd865a1bcd30fd513561e8672d215e6115
Travis CI User <travis@example.org>
2019-11-02 00:28:39 +0000

    deploy project

---
+++ js/precache-manifest.cb1d642c147e8c42c13f7f9f9b56cbff.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d6d4fc5ed3871adbf41e",
+    "url": "/pwa-vue-cli-demo/css/Home.ee61bb36.css"
+  },
+  {
+    "revision": "6b32b16277c606edec59",
+    "url": "/pwa-vue-cli-demo/css/about.1c4a2a0f.css"
+  },
+  {
+    "revision": "85bfc5b15928dee6933a",
+    "url": "/pwa-vue-cli-demo/css/app.2b8f2e11.css"
+  },
+  {
+    "revision": "82b9c7a5a3f405032b1db71a25f67021",
+    "url": "/pwa-vue-cli-demo/img/logo.82b9c7a5.png"
+  },
+  {
+    "revision": "b89032a4a5a1879f30ba05a13947f26f",
+    "url": "/pwa-vue-cli-demo/img/msapplication-icon-144x144.png"
+  },
+  {
+    "revision": "d6d4fc5ed3871adbf41e",
+    "url": "/pwa-vue-cli-demo/js/Home.646ff6b6.js"
+  },
+  {
+    "revision": "6b32b16277c606edec59",
+    "url": "/pwa-vue-cli-demo/js/about.b2157923.js"
+  },
+  {
+    "revision": "85bfc5b15928dee6933a",
+    "url": "/pwa-vue-cli-demo/js/app.9deba574.js"
+  },
+  {
+    "revision": "8090b8121bcf5b02a6f0",
+    "url": "/pwa-vue-cli-demo/js/chunk-vendors.96682519.js"
+  },
+  {
+    "revision": "04befa753aabe7339f70494fd102b17d",
+    "url": "/pwa-vue-cli-demo/js/manifest.json"
+  }
+]);
